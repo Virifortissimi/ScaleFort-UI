60dc5b8d116b139033c9c97cf4f13baafa723f0f
Gabriel Emmanuel <contacteghosa@gmail.com>
2025-01-12 20:26:45 +0100

    made the navbar functional

--- src/app/components/footer/footer-contact.component.ts
+++ src/app/components/footer/footer-contact.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
     <div>
       <h4 class="text-lg font-semibold mb-4">Contact</h4>
       <ul class="space-y-2 text-gray-400">
-        <li>Email: info&#64;scalefort.org</li>
-        <li>Phone: +234 815 840 6306</li>
+        <li>Email: <a href="mailto:info@scalefort.org">info&#64;scalefort.org</a></li>
+        <li>Whatsapp: <a href="https://wa.me/message/H6MHZGJ2FLYHM1">+234 815 840 6306</a></li>
       </ul>
     </div>
   `
