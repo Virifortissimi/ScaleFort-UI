e4a870874523df3554b9fc2d9f945501d77935dd
Gabriel Emmanuel <contacteghosa@gmail.com>
2025-01-12 10:25:30 +0100

    made the navbar functional

--- src/app/components/shared/slide-menu/slide-menu.component.ts
+++ src/app/components/shared/slide-menu/slide-menu.component.ts
@@ -57,6 +57,6 @@ export class SlideMenuComponent {
     { path: '/mentorship', text: 'Mentorship' },
     { path: '/courses', text: 'Courses' },
     { path: '/faq', text: 'FAQ' },
-    { path: '/get-started', text: 'Get Statrted' }
+    { path: '/get-started', text: 'Get Started' }
   ];
 }
