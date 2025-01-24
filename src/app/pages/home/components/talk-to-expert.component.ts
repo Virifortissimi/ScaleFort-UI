035a2cd11e4ccf8c4503322e5f0d41a674845217
Gabriel Emmanuel <contacteghosa@gmail.com>
2025-01-24 18:39:55 +0100

    made updates to the home page

--- src/app/pages/home/components/talk-to-expert.component.ts
+++ src/app/pages/home/components/talk-to-expert.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   template: `
     <section class="p-12 bg-gray-50">
         <div class="max-w-7xl mx-auto p-6">
-            <div class="bg-blue-300 text-grey-50 rounded-lg p-24 relative">
+            <div class="bg-blue-300 text-grey-50 rounded-lg p-24 talk-to-expert relative">
             
             <!-- Content -->
             <h3 class="text-lg font-semibold text-xl text-gray-600 mb-8 mb-2">Need help choosing a course?</h3>
