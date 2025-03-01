20276e813957be6fcee77cee358e11fda7b9fd8b
Gabriel Emmanuel <contacteghosa@gmail.com>
2025-03-01 11:47:55 +0100

    voucher page add

--- src/app/pages/community/components/community-events.component.ts
+++ src/app/pages/community/components/community-events.component.ts
@@ -14,12 +14,12 @@ import { Component } from '@angular/core';
       <!-- Course 1: Frontend Web Development -->
       <div class="bg-white p-6 rounded-lg shadow-xl hover:shadow-2xl transition-all duration-300 ease-in-out transform hover:scale-105">
         <div class="w-full mb-6">
-          <img src="https://res.cloudinary.com/virifortissimi/image/upload/v1737717982/Goals/undraw_blog_1ca8.svg" alt="Frontend Development Icon" class="w-full h-48 object-cover rounded-lg shadow-md">
+          <img src="https://res.cloudinary.com/virifortissimi/image/upload/v1737717982/Goals/undraw_blog_1ca8.svg" alt="Wordpress Development Icon" class="w-full h-48 object-cover rounded-lg shadow-md">
         </div>
         <div>
-          <h3 class="text-2xl font-semibold text-blue-600 mb-4">Frontend Web Development</h3>
-          <p class="text-gray-700 mb-4">Learn HTML, CSS, and JavaScript to build stunning, interactive websites. Start your journey as a web developer with the fundamental skills required in the industry.</p>
-          <p class="text-gray-800 mb-4"><strong>Next Cohort:</strong> March 1st, 2025 <br><strong>Weekend Classes:</strong> Saturdays <br><strong class="text-red-500">Price:</strong> <del class="text-gray-500">350,000</del> <span class="font-bold text-blue-600">N299,999</span></p>
+          <h3 class="text-2xl font-semibold text-blue-600 mb-4">Wordpress Development</h3>
+          <p class="text-gray-700 mb-4">Master WordPress development and build powerful, customized websites. Build a professional website or e-commerce store. No coding required! 🚀</p>
+          <p class="text-gray-800 mb-4"><strong>Next Cohort:</strong> TBD <br><strong>Weekend Classes:</strong> Saturdays <br><strong class="text-red-500">Price:</strong> <del class="text-gray-500">250,000</del> <span class="font-bold text-blue-600">N199,999</span></p>
           <a href="https://paystack.com/buy/scalefort?discount=LEARN2SCALE2025" target="_blank" class="block text-center bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition-all duration-200">Apply Now →</a>
         </div>
       </div>
@@ -45,7 +45,7 @@ import { Component } from '@angular/core';
         <div>
           <h3 class="text-2xl font-semibold text-blue-600 mb-4">Customer Support/Success</h3>
           <p class="text-gray-700 mb-4">Learn how to provide top-notch customer service and support for technology products. Develop key skills to thrive in customer success roles, from communication to problem-solving.</p>
-          <p class="text-gray-800 mb-4"><strong>Next Cohort:</strong> March 1st, 2025 <br><strong>Weekend Classes:</strong> Saturdays <br><strong class="text-red-500">Price:</strong> <del class="text-gray-500">350,000</del> <span class="font-bold text-blue-600">N299,999</span></p>
+          <p class="text-gray-800 mb-4"><strong>Next Cohort:</strong> TBD <br><strong>Weekend Classes:</strong> Saturdays <br><strong class="text-red-500">Price:</strong> <del class="text-gray-500">250,000</del> <span class="font-bold text-blue-600">N199,999</span></p>
           <a href="https://paystack.com/buy/scalefort?discount=LEARN2SCALE2025" target="_blank" class="block text-center bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition-all duration-200">Apply Now →</a>
         </div>
       </div>
